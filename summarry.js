4a091ac5f61be839f8ccd01481057a47d216ad40
Atikur Rhaman <designletter999@gmail.com>
2024-08-27 03:30:13 +0600

    3nd-edit

---
+++ summarry.js
@@ -0,0 +1,12 @@
+function onePlusAvg(x, y) {
+    return 1 + (x + y) / 2;
+}
+
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+console.log("One plus Average of a and b is ", 1 +(a + b)/2)
+console.log("One plus Average of a and c is ", 1 +(b + c)/2)
+console.log("One plus Average of a and c is ", 1 +(c + a)/2)
