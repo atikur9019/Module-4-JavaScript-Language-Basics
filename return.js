7e88ba45be4b6c72079ff3d1407501bf1515a7cd
Atikur Rhaman <designletter999@gmail.com>
2024-08-27 02:29:08 +0600

    2nd-edit

---
+++ return.js
@@ -0,0 +1,12 @@
+// var sum1 = 20;
+// var sum2 = 5;
+
+function add(sum1, sum2) {
+    var sum = sum1 + sum2;
+    return sum;
+    // return sum1 + sum2;
+}
+
+var add2 = add(20, 5);
+// console.log(add(20, 5));
+console.log(add2);
