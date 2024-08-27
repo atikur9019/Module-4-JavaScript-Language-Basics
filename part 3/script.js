771f8b5fbf52094eca51719815dc43170173067c
Atikur Rhaman <designletter999@gmail.com>
2024-08-28 00:53:41 +0600

    ses

--- part 3/script.js
+++ part 3/script.js
@@ -17,3 +17,12 @@ console.log(currentYear / 10);
 
 var rahimOlder = ageAtikur > ageRahim;
 console.log(rahimOlder);
+
+// Typeof operator
+
+console.log(typeof rahimOlder);
+console.log(typeof ageRahim);
+console.log(typeof 'Atikur');
+
+var x;
+console.log(typeof x);
