86ba462431fe0702338479d501001dc1c186126e
Atikur Rhaman <designletter999@gmail.com>
2024-08-28 00:41:20 +0600

    Edit

--- part 3/script.js
+++ part 3/script.js
@@ -1,7 +1,19 @@
-var currentYear = 2024;
+var currentYear, birthYearAtikur, birthYearRohim;
 
-var birthYearAtikur = currentYear - 14;
-var birthYearRahim = currentYear - 11;
+currentYear = 2024;
+ageAtikur = 14;
+ageRahim = 11;
+
+// MAth operator
+birthYearAtikur = currentYear - 14;
+birthYearRahim = currentYear - 11;
 
 console.log(birthYearAtikur);
 console.log(birthYearRahim);
+
+console.log(currentYear / 10);
+
+// Logical operator
+
+var rahimOlder = ageAtikur > ageRahim;
+console.log(rahimOlder);
