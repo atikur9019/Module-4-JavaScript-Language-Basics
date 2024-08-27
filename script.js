023b2c7c1eb303f1f7e683a632d2cee743340efd
Atikur Rhaman <designletter999@gmail.com>
2024-08-27 22:25:41 +0600

    3nd

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 // console.log('Hello Word')
 var firstName = 'Atikur';
-console.log(firstName);
+console.log(typeof firstName);
 
 var lastName = 'Rhaman';
 var age = 26;
+console.log(typeof age);
 
-Data Type:
-<<<<<<< Tabnine <<<<<<<
-var dataType = {//+
-    string: 'String',//+
-    number: 'Number',//+
-    boolean: 'Boolean',//+
-    undefined: 'undefined',//+
-    null: 'null'//+
-};//+
->>>>>>> Tabnine >>>>>>>
--String
--Number
--Boolean
--undefined
--null
+var fullAge = true;
+console.log(typeof fullAge);
+
+
+var job;
+console.log(typeof job);
+
+
+var job = 'Software Developer';
+console.log(job);
+
+
+// Data Type:
+// -String
+// -Number
+// -Boolean
+// -undefined
+// -null
