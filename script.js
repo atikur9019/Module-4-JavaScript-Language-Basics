6c04d2dc40d036553d7d6846cea94fab96ced6d8
Atikur Rhaman <designletter999@gmail.com>
2024-08-27 22:05:08 +0600

    1st

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello Word')
+// console.log('Hello Word')
+var firstName = 'Atikur';
+console.log(firstName);
+
+var lastName = 'Rhaman';
+var age = 26;
+
+Data Type:
+<<<<<<< Tabnine <<<<<<<
+var dataType = {//+
+    string: 'String',//+
+    number: 'Number',//+
+    boolean: 'Boolean',//+
+    undefined: 'undefined',//+
+    null: 'null'//+
+};//+
+>>>>>>> Tabnine >>>>>>>
+-String
+-Number
+-Boolean
+-undefined
+-null
