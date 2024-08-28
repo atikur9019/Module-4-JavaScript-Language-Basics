14ea7f71e39b7f54d297787424b0d074684fa73a
Atikur Rhaman <designletter999@gmail.com>
2024-08-28 22:01:33 +0600

    Edit-2

---
+++ part-5/script.js
@@ -0,0 +1,14 @@
+/************************
+ * Operator Precedence
+ */
+
+var now = 2024;
+var yearAtikur = 2009;
+var fullAge = 14;
+// Multiple Operators
+var isFullAge = now - yearAtikur >= fullAge;
+console.log(isFullAge);
+
+if (isFullAge <= 18) {
+    
+}
