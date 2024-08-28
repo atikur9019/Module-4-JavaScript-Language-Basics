9a28d4fb67bd20adb2dcd0697f841ccee49d4e9b
Atikur Rhaman <designletter999@gmail.com>
2024-08-28 21:47:50 +0600

    fast

--- part-4/script.js
+++ part-4/script.js
@@ -1,2 +1,3 @@
 // document.getElementById('atikur').innerHTML = 5 + 6;
 
+// document.write(5 + 6);
---
+++ part-4/script5.js
@@ -0,0 +1,7 @@
+var a, b, c;
+
+a = 10;
+b = 20;
+c = b - a;
+
+document.getElementById('atikur').innerHTML = c;
