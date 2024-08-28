4f708ea01d01edbe556c145c1ced1720bcef313e
Atikur Rhaman <designletter999@gmail.com>
2024-08-29 00:27:58 +0600

    boolean login-commit-1

---
+++ part-6/boolean.js
@@ -0,0 +1,14 @@
+// boolean Logic
+
+var firsName = 'Atikur';
+
+var age = 14;
+
+if( age < 13) {
+    alert(firsName + ' is a boy. ' );
+}else if( age > 13 && age < 20) {
+    alert(firsName + ' is a teenager. ' );
+}
+else {
+    alert(firsName +' is a man. ');
+}
