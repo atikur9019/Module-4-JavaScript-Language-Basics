16cde037546c41cbb6453062cd914dc31ed24771
Atikur Rhaman <designletter999@gmail.com>
2024-08-28 22:14:10 +0600

    hi

--- part-5/script.js
+++ part-5/script.js
@@ -7,8 +7,12 @@ var yearAtikur = 2009;
 var fullAge = 14;
 // Multiple Operators
 var isFullAge = now - yearAtikur >= fullAge;
-console.log(isFullAge);
+// console.log(isFullAge);
 
-if (isFullAge <= 18) {
-    
+if (isFullAge >= 18) {
+    console.log("Eligible to open an Atikur Shortsi account");
+}
+else{
+    console.log("Atikur is not eligible to open a Shortsi account");
+
 }
