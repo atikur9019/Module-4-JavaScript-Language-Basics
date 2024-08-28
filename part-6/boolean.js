43019d93aef762a52f756855fdbac451e088e7b5
Atikur Rhaman <designletter999@gmail.com>
2024-08-29 00:34:56 +0600

    end boolean logic

--- part-6/boolean.js
+++ part-6/boolean.js
@@ -8,6 +8,8 @@ if( age < 13) {
     alert(firsName + ' is a boy. ' );
 }else if( age > 13 && age < 20) {
     alert(firsName + ' is a teenager. ' );
+}else if( age >= 20 || age < 30) {
+     alert(firsName +' is a young man. ');
 }
 else {
     alert(firsName +' is a man. ');
